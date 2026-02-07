0063fa47d0639e198d0d7cc85da533b6264ec4ff
Åke Engelbrektson <eson57@users.noreply.github.com>
2026-02-07 13:05:49 +0100

    Create harbour-rainviewer-sv.ts
    Add Swedish translation

---
+++ translations/harbour-rainviewer-sv.ts
@@ -0,0 +1,211 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="42"/>
+        <source>Rainviewer %1</source>
+        <translation>Rainviewer %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="62"/>
+        <source>Simple map to display rainmaps from %1</source>
+        <translation>Enkel karta som visar regndata från %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="73"/>
+        <source>© %1 %2</source>
+        <translation>© %1 %2</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="82"/>
+        <source>This program is FOSS software licensed GNU General Public License v3.</source>
+        <translation>Detta program är FOSS, licensierat under GNU General Public License v3.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="90"/>
+        <source>Source: %1</source>
+        <translation>Källkod %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="104"/>
+        <source>If you encounter bugs or have feature requests, please file tickets in the Issue Tracker: %1</source>
+        <translation>Om du stöter på buggar eller har önskemål om funktioner, vänligen skapa ett ärende i ärendehanteraren: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="117"/>
+        <source>Buy me a beer</source>
+        <translation>Bjud mig på en öl</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="124"/>
+        <source>Homepage</source>
+        <translation>Hemsida</translation>
+    </message>
+</context>
+<context>
+    <name>ColorLegend</name>
+    <message>
+        <location filename="../qml/pages/ColorLegend.qml" line="35"/>
+        <source>Color legend</source>
+        <translation>Färgförklaring</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ColorLegend.qml" line="50"/>
+        <source>Rain</source>
+        <translation>Regn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ColorLegend.qml" line="53"/>
+        <source>Snow</source>
+        <translation>Snö</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ColorLegend.qml" line="265"/>
+        <source>Overcast</source>
+        <translation>Mulet</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ColorLegend.qml" line="268"/>
+        <source>Drizzle</source>
+        <translation>Duggregn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ColorLegend.qml" line="271"/>
+        <source>Light rain</source>
+        <translation>Lätt regn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ColorLegend.qml" line="274"/>
+        <source>Moderate rain</source>
+        <translation>Måttligt regn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ColorLegend.qml" line="277"/>
+        <source>Shower</source>
+        <translation>Strilregn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ColorLegend.qml" line="280"/>
+        <source>Hail</source>
+        <translation>Hällregn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ColorLegend.qml" line="290"/>
+        <source>Light</source>
+        <translation>Lätt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ColorLegend.qml" line="294"/>
+        <source>Moderate</source>
+        <translation>Måttligt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ColorLegend.qml" line="298"/>
+        <source>Heavy</source>
+        <translation>Tungt</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="52"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="53"/>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="54"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="59"/>
+        <source>Map settings</source>
+        <translation>Kartinställningaqr</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="64"/>
+        <source>Map style</source>
+        <translation>Kartstil</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="94"/>
+        <source>Enable GPS</source>
+        <translation>Aktivera GPS</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="95"/>
+        <source>Will enable gps tracking while the application is visible</source>
+        <translation>Aktiverar gps-spårning medan appen är öppen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="100"/>
+        <source>Rain settings</source>
+        <translation>Regninställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="105"/>
+        <source>Smooth Rain Rendering</source>
+        <translation>Mjuk regnrendering</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="111"/>
+        <source>Enable snow</source>
+        <translation>Aktivera snö</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="117"/>
+        <source>Rain resolution</source>
+        <translation>Regnupplösning</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="134"/>
+        <source>Rain color scheme</source>
+        <translation>Regnfärgsschema</translation>
+    </message>
+</context>
+<context>
+    <name>StartPage</name>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="49"/>
+        <source>Clear Map Cache</source>
+        <translation>Rensa kart-cachen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="51"/>
+        <source>Clearing map cache</source>
+        <translation>Tömmer kart-cachen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="57"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="61"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="65"/>
+        <source>Color legend</source>
+        <translation>Färgförklaring</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="73"/>
+        <source>Rainviewer</source>
+        <translation>Regnvisare</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="213"/>
+        <source>Scale: %0 m/pixel ; zoom: %1</source>
+        <translation>Skala: %0 m/pixel ; zoom: %1</translation>
+    </message>
+</context>
+</TS>
